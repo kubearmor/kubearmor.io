107e5cdbf28273bfba2a7919bef5eb47ca3543d2
Aditya Prasad Mohanty <adityaprasadmohanty150@gmail.com>
2023-11-06 08:36:52 +0530

    fix: deploy fix
    Signed-off-by: Aditya Prasad Mohanty <adityaprasadmohanty150@gmail.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "kubearmor", // Usually your GitHub org/user name.
-  projectName: "kubearmor.github.io", // Usually your repo name.
+  projectName: "kubearmor.io", // Usually your repo name.
   deploymentBranch: "main",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
