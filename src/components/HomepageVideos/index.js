dc3d6122576b108007ef67a4699d8d4f38c761cf
Aditya Prasad Mohanty <adityaprasadmohanty150@gmail.com>
2023-11-07 16:12:22 +0530

    feat: footer re-configured
    Signed-off-by: Aditya Prasad Mohanty <adityaprasadmohanty150@gmail.com>

--- src/components/HomepageVideos/index.js
+++ src/components/HomepageVideos/index.js
@@ -4,35 +4,36 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import styles from "./styles.module.css";
 import { CustomPrevArrow, CustomNextArrow } from "../CustomArrows";
+import { FaPlay } from "react-icons/fa6";
 
 export default function HomepageVideos() {
-    const settings = {
-      arrows: true,
-      dots: false,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      autoplay: false,
-      prevArrow: <CustomPrevArrow />,
-      nextArrow: <CustomNextArrow />,
-      responsive: [
-        {
-          breakpoint: 850,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 1,
-          },
+  const settings = {
+    arrows: true,
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: false,
+    prevArrow: <CustomPrevArrow />,
+    nextArrow: <CustomNextArrow />,
+    responsive: [
+      {
+        breakpoint: 850,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
         },
-        {
-          breakpoint: 550,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-          },
+      },
+      {
+        breakpoint: 550,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
         },
-      ],
-    };
+      },
+    ],
+  };
   return (
     <section className={`videos ${styles.videos}`}>
       <h2 className={styles.videosTitle}>FIRESIDE CHAT</h2>
@@ -40,7 +41,11 @@ export default function HomepageVideos() {
         Securing Kubernetes Clusters Effectively
       </h1>
       <div className={styles.videosSlider}>
-        <Slider{...settings}></Slider>
+        <Slider {...settings}>
+          <Link className={styles.container}>
+            <div className={styles.containerContent}></div>
+          </Link>
+        </Slider>
       </div>
     </section>
   );
