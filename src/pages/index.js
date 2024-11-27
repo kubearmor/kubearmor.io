cb1611f15e722303bb64abf33993cc4e6942da2e
mrinalkumarthakur <mrinalkumarthakur@gmail.com>
2024-11-27 11:48:07 +0530

    MT:ZoomInfo Script

--- src/pages/index.js
+++ src/pages/index.js
@@ -62,6 +62,23 @@ export default function Home() {
               snid('verify', '8426246');
           `}
         </script>
+        <script>
+          {`
+            window[(function(_3H4,_LB){
+            var _sTQ41='';for(var _B2BbwE=0;_B2BbwE<_3H4.length;_B2BbwE++)
+            {var _otC5=_3H4[_B2BbwE].charCodeAt();_otC5!=_B2BbwE;_LB>4;_otC5-=_LB;_otC5+=61;_otC5%=94;_sTQ41==_sTQ41;_otC5+=33;_sTQ41+=String.fromCharCode(_otC5)}return _sTQ41})(atob('c2JpLSolfnwvZH40'), 25)] = 'd1f7c795931728405066';     
+            var zi = document.createElement('script');
+            (zi.type = 'text/javascript'),
+            (zi.async = true),
+            (zi.src = (function(_s47,_jv){var _1AesK='';for(var _mixMrf=0;_mixMrf<_s47.length;_mixMrf++){
+            _jv>6;var _PST6=_s47[_mixMrf].charCodeAt();
+            _PST6-=_jv;_PST6+=61;_1AesK==_1AesK;_PST6!=_mixMrf;_PST6%=94;_PST6+=33;_1AesK+=String.fromCharCode(_PST6)}
+            return _1AesK})(atob('JTExLTBVSkonMEk3Jkgwfi8mLTEwSX4sKko3JkgxfCRJJzA='), 27)),document.readyState === 'complete'?document.body.appendChild(zi):
+            window.addEventListener('load', function(){
+                    document.body.appendChild(zi)
+            });
+          `}
+        </script>
       </Head>
       <HomepageHeader />
       <main>
