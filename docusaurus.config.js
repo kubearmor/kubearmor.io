341a54d472d27050aa7f42ef94acc44741ae5e8e
Atharva Shah <highnessatharva@gmail.com>
2026-06-29 18:28:23 +0530

    update baseurl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,7 +4,7 @@
 const lightCodeTheme = require("prism-react-renderer/themes/github");
 const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 const url = "https://kubearmor.io";
-const baseUrl = "/kubearmor.io/";
+const baseUrl = "/"
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
